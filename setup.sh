a6a69dcbe8254e0fdba11e0975df41c64c43e245
bichiko <b_kodua@cu.edu.ge>
2019-03-09 01:00:43 +0200

    lets see

--- setup.sh
+++ setup.sh
@@ -9,20 +9,35 @@ a2enmod rewrite
 sed -i -e 's/AllowOverride None/AllowOverride All/g' /etc/apache2/apache2.conf
 #restart apache
 systemctl restart apache2
-cp /var/www/html/index.html /var/www/index.html
-#cut and move all webserver files to web root directory
-rm -rf /var/www/html/*
-mv /var/www/index.html /var/www/html/index.html
-cp -r /root/setup/ITI0103-applying-defense/webserver/* /var/www/html/
-rm -f /var/www/html/setup.sh
-#including hidden files
-cp -r /root/setup/ITI0103-applying-defense/webserver/.* /var/www/html/
-#remove it
-rm -rf /root/setup/ITI0103-applying-defense/
+a=`ifconfig | grep -o "192.168.8.254"`
 
-php /var/www/html/setupdb.php >> /dev/null
-#remove loading spinner
-rm -f /var/www/html/setup.sh
-rm -f /var/www/html/setupdb.php
-rm -f /var/www/html/index.html
-rm -f /var/www/html/README.md
+if [ a != "" ]; then
+#this is machine for check
+    #this opens in users machine
+    echo "Please visit 192.168.8.254" >> /var/www/html/index.html
+    echo "<br>I am going to check your work bitch" >> /var/www/html/index.html
+else
+	#create ssh user ninja:ninja
+	#this is web server
+	printf 'ninja\nninja\n\n\n\n\n\n\n\n' | adduser --home /var/www/html --shell /bin/bash --no-create-home --ingroup www-data --ingroup ssh ninja
+	#remove it
+	rm -f iti0103-web-applying-defense.sh
+	#vulnerable machine
+	cp /var/www/html/index.html /var/www/index.html
+	#cut and move all webserver files to web root directory
+	rm -rf /var/www/html/*
+	mv /var/www/index.html /var/www/html/index.html
+	cp -r /root/setup/ITI0103-applying-defense/webserver/* /var/www/html/
+	rm -f /var/www/html/setup.sh
+	#including hidden files
+	cp -r /root/setup/ITI0103-applying-defense/webserver/.* /var/www/html/
+	#remove it
+	rm -rf /root/setup/ITI0103-applying-defense/
+
+	php /var/www/html/setupdb.php >> /dev/null
+	#remove loading spinner
+	rm -f /var/www/html/setup.sh
+	rm -f /var/www/html/setupdb.php
+	rm -f /var/www/html/index.html
+	rm -f /var/www/html/README.md
+fi
