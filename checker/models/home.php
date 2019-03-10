62246fca007b2ba62d8e2d43a032061e5000866c
bichiko <b_kodua@cu.edu.ge>
2019-03-10 02:52:37 +0200

    fix syntax

--- checker/models/home.php
+++ checker/models/home.php
@@ -38,7 +38,7 @@ class HomeModel extends Model{
                 }elseif ($task === 2) {
 
                     $resp = json_decode(Curl::send(TARGET.'/home/ajax', ['action' => 'search', 'search' => "___'or'1=1"]))->{"data"};
-                    if( gettype($resp) === "string" && count($resp) === 0){
+                    if( gettype($resp) === "array" && count($resp) === 0){
                         $status = 'fixed';
                     }
                    
